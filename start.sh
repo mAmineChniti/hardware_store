43a21b1bbc3bbd3ac46d89c2219bef3efd764d0f
mAmineChniti <emin.chniti@esprit.tn>
2026-07-29 16:48:01 +0100

    render blueprint

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -z "$SPRING_DATASOURCE_URL" ] && [ -n "$DB_HOSTNAME" ] && [ -n "$DB_PORT" ] && [ -n "$DB_NAME" ]; then
+if [ -n "$DB_HOSTNAME" ] && [ -n "$DB_PORT" ] && [ -n "$DB_NAME" ]; then
   export SPRING_DATASOURCE_URL="jdbc:postgresql://${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}"
 fi
 exec java -jar app.jar
