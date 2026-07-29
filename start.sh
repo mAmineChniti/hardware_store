760513f28f34664bcb36845029ad3a018b28fb0d
mAmineChniti <emin.chniti@esprit.tn>
2026-07-29 16:53:15 +0100

    render blueprint

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+echo "DB_HOSTNAME=$DB_HOSTNAME"
+echo "DB_PORT=$DB_PORT"
+echo "DB_NAME=$DB_NAME"
+unset SPRING_DATASOURCE_URL
 if [ -n "$DB_HOSTNAME" ] && [ -n "$DB_PORT" ] && [ -n "$DB_NAME" ]; then
   export SPRING_DATASOURCE_URL="jdbc:postgresql://${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}"
 fi
+echo "SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL"
 exec java -jar app.jar
